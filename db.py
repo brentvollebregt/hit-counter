ce2495b9ee3b29b4d8c327cc0b34f73b908ac776
Brent Vollebregt <volleys2468@gmail.com>
2018-04-23 23:27:32 +1200

    Cleaning and notes

--- db.py
+++ db.py
@@ -4,25 +4,16 @@ import time
 
 
 class DbAccess:
-    """ This provides access to the database to keep track of views
-    Tables:
-        - URLs (keeps track of urls and counts)
-            - id
-            - url
-            - count
-        - Views (stops the refresh issue for a set amount of time)
-            - id
-            - URL_id
-            - value (provided from cookie)
-            - time
-    """
+    """ This provides access to the database to keep track of urls and views """
     def __init__(self, filename):
+        """ Setup connection to file and create tables if they don't exist"""
         self.connection = lite.connect(filename)
         self.cursor = self.connection.cursor()
         self.cursor.execute('CREATE TABLE IF NOT EXISTS url (id INTEGER PRIMARY KEY, url VARCHAR(256), count INTEGER);')
         self.cursor.execute('CREATE TABLE IF NOT EXISTS views (id INTEGER PRIMARY KEY, url_id INTEGER, value VARCHAR(64), time INTEGER, FOREIGN KEY (url_id) REFERENCES url(id));')
 
     def getCount(self, url):
+        """ Get the count of a particular url """
         self.cursor.execute('SELECT count FROM url WHERE url=?', (url,))
         data = self.cursor.fetchone()
         if data is None:
@@ -31,12 +22,13 @@ class DbAccess:
             return data[0]
 
     def addView(self, url, value):
+        """ Create url entry if needed and increase url count and add cookie value to views if value is not stored """
         # Make sure the url entry exists
         count = self.getCount(url)
         if count == 0:
             self.cursor.execute('INSERT INTO url(url, count) VALUES(?, ?)', (url, 0))
         # Get id of the url we are coutning
-        self.cursor.execute('SELECT id FROM url WHERE url=?', (url,))
+        self.cursor.execute('SELECT id FROM url WHERE url=?', (url, ))
         url_id = self.cursor.fetchone()[0]
         # Return if cookie value already here for the url
         self.cursor.execute('SELECT * FROM views WHERE url_id=? AND value=?', (url_id, value))
@@ -45,12 +37,13 @@ class DbAccess:
         # Add 1 to the url count
         self.cursor.execute('UPDATE url SET count = count + 1 WHERE id=?', (url_id, ))
         # Add this view to the table with a timeout
-        cookie_expiration = time.time() + config.COOKIE_TIMEOUT
+        cookie_expiration = round(time.time() + config.COOKIE_TIMEOUT)
         self.cursor.execute('INSERT INTO views(url_id, value, time) VALUES(?, ?, ?)', (url_id, value, cookie_expiration))
 
     def commit(self):
+        """ Commit """
         self.connection.commit()
 
     def clean(self):
-        # Remove items that are less than time.time()
-        pass
+        """ Remove view rows that have expired """
+        self.cursor.execute('DELETE FROM views WHERE time < ?', (time.time(), ))
--- utils.py
+++ utils.py
@@ -4,17 +4,20 @@ import string
 
 
 def getSVG(count):
+    """ Put the count in the pre-defined svg and return it """
     return config.SVG_TEMPLATE.format(count=count)
 
 def getURL(request):
+    """ Get the url out of a request either passed as a query parameter or taken from the referrer """
     return request.args.get('url', request.referrer)
 
 def getCookie(request, url):
-    # return request.cookies[url] if url in request.cookies else randomValue()
+    """ Get the cookie out of the request relative to the url provided or generate a new value if it doesn't exist"""
     if url in request.cookies:
         return request.cookies[url]
     else:
         return randomValue()
 
 def randomValue():
+    """ Generate a random string from upper and lowercase letters and digits of a define length """
     return ''.join([random.choice(string.ascii_letters + string.digits) for i in range(config.RANDOM_VALUE_LENGTH)])
