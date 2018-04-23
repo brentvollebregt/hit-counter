78507ec2b69e7ee71d8510da87eac66083621f7a
Brent Vollebregt <volleys2468@gmail.com>
2018-04-23 21:39:16 +1200

    /count complete

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import config
 import sqlite3 as lite
+import time
 
 
 class DbAccess:
@@ -15,15 +16,34 @@ class DbAccess:
             - value (provided from cookie)
             - time
     """
-    def __init__(self):
-        connection = lite.connect(config.DATABASE_FILENAME)
-        self.cursor = connection.cursor()
+    def __init__(self, filename):
+        self.connection = lite.connect(filename)
+        self.cursor = self.connection.cursor()
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS url (id INTEGER PRIMARY KEY, url VARCHAR(256), count INTEGER);')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS views (id INTEGER PRIMARY KEY, url_id INTEGER, value VARCHAR(64), time INTEGER, FOREIGN KEY (url_id) REFERENCES url(id));')
 
-    def getUrl(self, url):
-        pass
+    def getCount(self, url):
+        self.cursor.execute('SELECT count FROM url WHERE url=?', (url,))
+        data = self.cursor.fetchone()
+        if data is None:
+            return 0
+        else:
+            return data[0]
 
     def addView(self, url, value):
-        pass
+        count = self.getCount(url)
+        if count == 0:
+            self.cursor.execute('INSERT INTO url(url, count) VALUES(?, ?)', (url, 0))
+        # TODO Check if cookie value already there
+        self.cursor.execute('SELECT id FROM url WHERE url=?', (url,))
+        url_id = self.cursor.fetchone()[0]
+        self.cursor.execute('UPDATE url SET count = count + 1 WHERE id=?', (url_id,))
+        cookie_expiration = time.time() + config.COOKIE_TIMEOUT
+        self.cursor.execute('INSERT INTO views(url_id, value, time) VALUES(?, ?, ?)', (url_id, value, cookie_expiration))
+
+    def commit(self):
+        self.connection.commit()
 
     def clean(self):
+        # Remove items that are less than time.time()
         pass
--- server.py
+++ server.py
@@ -1,29 +1,54 @@
+import config
 import db
-from flask import Flask
+import utils
+from flask import Flask, request, send_file, make_response
 
 app = Flask(__name__, static_url_path='')
+db_connection = db.DbAccess(config.DATABASE_FILENAME)
 
 @app.route("/")
 def homeRoute():
     """ Home + tool to create (nocount/count + url in url) """
-    pass
+    render_template('index.html')
+
 
 @app.route("/count")
-def homeRoute():
+def countRoute():
     """ Return the count for a url and add 1 to it """
-    pass
+    url = utils.getURL(request)
+    if url is None:
+        return "", 404
+
+    cookie = utils.getCookie(request, url)
+    db_connection.addView(url, cookie)
+    count = db_connection.getCount(url)
+    db_connection.commit()
+
+    response = make_response(str(count), 200)
+    response.set_cookie(url, cookie)
+    return response
 
 @app.route("/count/tag.svg")
-def homeRoute():
+def countTagRoute():
     """ Return svg of count and add 1 to url """
+    # svg = StringIO()
+    # svg.write(getSVG(count))
+    # svg.seek(0)
+    # return send_file(svg, mimetype='image/svg+xml')
     pass
 
 @app.route("/nocount")
-def homeRoute():
+def nocountRoute():
     """ Return the count for a url """
     pass
 
 @app.route("/nocount/tag.svg")
-def homeRoute():
+def nocountTagRoute():
     """ Return svg of count """
     pass
+
+if __name__ == '__main__':
+    ip = '127.0.0.1'
+    port = 8080
+    print("Site starting on http://" + ip + ":" + str(port))
+    app.run(host=ip, port=port)
