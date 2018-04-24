13813130af19fc46f82a9af1b1c56499dd72a875
Brent Vollebregt <volleys2468@gmail.com>
2018-04-24 23:37:12 +1200

    Disable caching

--- server.py
+++ server.py
@@ -80,6 +80,16 @@ def nocountTagRoute():
 
     return makeSVGRequest(count, url, None)
 
+@app.after_request
+def add_header(r):
+    """
+    Disable caching - https://stackoverflow.com/questions/34066804/disabling-caching-in-flask
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    return r
+
 if __name__ == '__main__':
     ip = '127.0.0.1'
     port = 8080
