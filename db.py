e7ed534c5d707868b133341b9b9cf53e975ddf65
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2020-07-15 18:45:47 +0200

    fix: attempt to improve performance by augmenting sql queries

--- db.py
+++ db.py
@@ -1,33 +1,44 @@
 import os
-import sqlite3 as lite
-import config
-from collections import defaultdict
 import re
+import sqlite3 as lite
+
+import config
 
 class DbAccess:
     """ This provides access to the database to keep track of urls and views """
+
     def __init__(self, filename):
         """ Setup connection to file and create tables if they don't exist"""
         self.filename = filename
+        self.connection = None
 
         # Create folder for database file if it doesn't already exist
         if not os.path.exists(os.path.dirname(filename)):
             print('WARN: The parent directory for ' + filename + ' does not exist so it will be created.')
             os.makedirs(os.path.dirname(filename), exist_ok=True)
 
-        connection = lite.connect(filename)
-        connection.execute('pragma journal_mode=wal')
-        cursor = connection.cursor()
+        self.connect()
+        cursor = self.connection.cursor()
         cursor.execute('CREATE TABLE IF NOT EXISTS url (id INTEGER PRIMARY KEY, url VARCHAR(256), count INTEGER);')
+
+    def connect(self):
+        def regexp(expr, item):
+            reg = re.compile(expr)
+            return reg.search(item) is not None
+
+        self.connection = lite.connect(self.filename, check_same_thread=False)
+        self.connection.execute('pragma journal_mode=wal')
+        self.connection.create_function('REGEXP', 2, regexp)
 
     def get_connection(self):
         """ Get the cursor to use in the current thread and remove rows that have expired in views"""
-        connection = lite.connect(self.filename)
-        return connection
+        if self.connection is None:
+            self.connect()
+        return self.connection
 
-    def getCount(self, connection, url):
+    def getCount(self, url):
         """ Get the count of a particular url """
-        cursor = connection.cursor()
+        cursor = self.connection.cursor()
         cursor.execute('SELECT count FROM url WHERE url=?', (url,))
         data = cursor.fetchone()
         if data is None:
@@ -35,68 +46,69 @@ class DbAccess:
         else:
             return data[0]
 
-    def addView(self, connection, url):
+    def addView(self, url):
         """ Create url entry if needed and increase url count and add cookie value to views if value is not stored """
-        cursor = connection.cursor()
+        cursor = self.connection.cursor()
         # Make sure the url entry exists
-        count = self.getCount(connection, url)
+        count = self.getCount(url)
         if count == 0:
             cursor.execute('INSERT INTO url(url, count) VALUES(?, ?)', (url, 0))
         # Add 1 to the url count
-        cursor.execute('UPDATE url SET count = count + 1 WHERE url=?', (url, ))
-        connection.commit()
+        cursor.execute('UPDATE url SET count = count + 1 WHERE url=?', (url,))
+        self.connection.commit()
 
-    def getTopSites(self, connection, amount=10):
+    def getTopSites(self, amount=10):
         """ Get the top domains using this tool by hits. Ignore specified domains """
-        top_urls = self.getTopUrls(connection, None)
+        return self.getTop(amount, 'domains')
 
-        # Get total hits per domain
-        site_counts = defaultdict(int)
-        for url in top_urls['urls']:
-            # Get the domain - part before the first '/'
-            domain = url.split('/')[0]
-            # Add hit counts to the domain
-            site_counts[domain] += top_urls['values'][url]
-
-        # Sort the domains by hits
-        sorted_sites = sorted(site_counts, key=lambda x: site_counts[x], reverse=True)
-
-        # Return sorted domains and their values, this allows for lower Python version support
-        return {
-            'domains': sorted_sites[:amount],
-            'values': {site: site_counts[site] for site in site_counts}
-        }
-
-    def getTopUrls(self, connection, amount=10):
+    def getTopUrls(self, amount=10):
         """ Get the top urls using this tool by hits. Ignore specified domains """
-        # Select all urls and counts
-        cursor = connection.cursor()
-        cursor.execute('select url, count from url')
-        urls_and_counts = cursor.fetchall()
+        return self.getTop(amount, 'urls')
 
-        # Get total hits per url
-        url_counts = defaultdict(int)
-        for row in urls_and_counts:
-            url = row[0]
+    def getTop(self, amount, what='domains'):
+        query = self._top_urls_query() if what == 'urls' else self._top_domains_query()
+        # Select all entities and counts
+        cursor = self.connection.cursor()
+        cursor.execute(
+            query,
+            [*[str(r) for r in config.TOP_SITES_IGNORE_DOMAIN_RE_MATCH], amount]
+        )
+        result = cursor.fetchall()
+
+        entities, values = [], {}
+        for row in result:
+            url, domain, count = row
             if url == b'':
                 continue
-            # Check if url is on the ignore list
-            on_ignore = False
-            for regex in config.TOP_SITES_IGNORE_DOMAIN_RE_MATCH:
-                # Only match against the domain part
-                if re.match(regex, url.split('/')[0]) is not None:
-                    on_ignore = True
-                    break
-            if on_ignore:
-                continue
-            # Add hit counts to the url
-            url_counts[row[0]] += row[1]
+            entities.append(url)
+            values[url] = count
 
-        # Sort the urls by hits
-        sorted_urls = sorted(url_counts, key=lambda x: url_counts[x], reverse=True)
-
-        # Return sorted urls and their values, this allows for lower Python version support
+        # Return sorted entities and their values, this allows for lower Python version support
         return {
-            'urls': sorted_urls[:amount],
-            'values': {url: url_counts[url] for url in url_counts}
+            what: entities,
+            'values': values
         }
+
+    def _top_domains_query(self):
+        sep = '\nAND '
+        return f"""
+            SELECT url, substr(url, 0, instr((url || '/'), '/')) as domain, SUM(count) as domain_sum
+            FROM url
+            WHERE domain != ''
+            GROUP BY domain
+            HAVING {sep.join(['domain NOT REGEXP ?' for _ in range(len(config.TOP_SITES_IGNORE_DOMAIN_RE_MATCH))])}
+            ORDER BY domain_sum DESC
+            LIMIT ?;
+        """
+
+    def _top_urls_query(self):
+        sep = '\nAND '
+        return f"""
+            SELECT url, substr(url, 0, instr((url || '/'), '/')) as domain, SUM(count) as url_sum
+            FROM url
+            WHERE domain != ''
+            GROUP BY url
+            HAVING {sep.join(['domain NOT REGEXP ?' for _ in range(len(config.TOP_SITES_IGNORE_DOMAIN_RE_MATCH))])}
+            ORDER BY url_sum DESC
+            LIMIT ?;
+        """
--- server.py
+++ server.py
@@ -31,11 +31,10 @@ def makeSVGRequest(count, url, cookie_required):
 @app.route("/")
 def homeRoute():
     """ Home + tool to create (nocount/count + url in url) """
-    connection = db_connection.get_connection()
     return render_template(
         'index.html',
-        top_sites=db_connection.getTopSites(connection, config.NUM_TOP_DOMAINS),
-        top_urls=db_connection.getTopUrls(connection, config.NUM_TOP_URLS),
+        top_sites=db_connection.getTopSites(config.NUM_TOP_DOMAINS),
+        top_urls=db_connection.getTopUrls(config.NUM_TOP_URLS),
         show_sites=config.NUM_TOP_DOMAINS > 0,
         show_urls=config.NUM_TOP_URLS > 0
     )
@@ -53,10 +52,9 @@ def countRoute():
 
     # Get/generate cookie, cleanup views, add a view, get the count and commit changes
     valid_cookie = utils.checkValidCookie(request, url)
-    connection = db_connection.get_connection()
     if not valid_cookie:
-        db_connection.addView(connection, url)
-    count = db_connection.getCount(connection, url)
+        db_connection.addView(url)
+    count = db_connection.getCount(url)
 
     return makeTextRequest(count, url, not valid_cookie)
 
@@ -71,10 +69,9 @@ def countTagRoute():
         return config.FORBIDDEN_URL_MESSAGE, 403
 
     valid_cookie = utils.checkValidCookie(request, url)
-    connection = db_connection.get_connection()
     if not valid_cookie:
-        db_connection.addView(connection, url)
-    count = db_connection.getCount(connection, url)
+        db_connection.addView(url)
+    count = db_connection.getCount(url)
 
     return makeSVGRequest(count, url, not valid_cookie)
 
@@ -88,8 +85,7 @@ def nocountRoute():
     if not utils.checkURLWhitelist(url):
         return config.FORBIDDEN_URL_MESSAGE, 403
 
-    connection = db_connection.get_connection()
-    count = db_connection.getCount(connection, url)
+    count = db_connection.getCount(url)
 
     return makeTextRequest(count, url, False)
 
@@ -103,8 +99,7 @@ def nocountTagRoute():
     if not utils.checkURLWhitelist(url):
         return config.FORBIDDEN_URL_MESSAGE, 403
 
-    connection = db_connection.get_connection()
-    count = db_connection.getCount(connection, url)
+    count = db_connection.getCount(url)
 
     return makeSVGRequest(count, url, False)
 
